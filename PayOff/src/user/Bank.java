62ba32b7d55840f3c6fc125251fd68a28c5c2073
Juan Carlo Pascua <carlo.pascua@gmail.com>
2018-02-18 18:35:32 -0800

    Created bank input at the prompt for Bank class

--- PayOff/src/user/Bank.java
+++ PayOff/src/user/Bank.java
@@ -1,34 +1,87 @@
 package user;
 
+import java.util.Scanner;
+
 public class Bank {
-	private String bankName;
-	private double bankBalance;
-	private double bankAPR;
+	private String bankName = "";
+	private double bankBalance = 0;
+	private double bankAPR = 0;
+	private static java.util.Scanner input;
 	
 	//constructor, three parameter for Bank object
-	public Bank (String bankName, double bankBalance, double bankAPR) {
+	public Bank () 
+	{
 		this.bankName = bankName;
 		this.bankBalance = bankBalance; 
 		this.bankAPR = bankAPR;
 	}
 	
+	//constructor, three parameter for Bank object
+	public Bank (String bankName, double bankBalance, double bankAPR) 
+	{
+		this.bankName = bankName;
+		this.bankBalance = bankBalance; 
+		this.bankAPR = bankAPR;
+	}
+	
+	//setter methods
+	public void setBankName(String bankName) 
+	{
+		this.bankName = bankName;
+	}
+	public void setBankBalance(double bankBalance) 
+	{
+		this.bankBalance = bankBalance;
+	}
+	public void setBankAPR(double bankAPR) 
+	{
+		this.bankAPR = bankAPR;
+	}
+	
 	//getter methods
-	public String getBankName() {
+	public String getBankName() 
+	{
 		return this.bankName;
 	}
-	public double getBankBalance() {
+	public double getBankBalance() 
+	{
 		return this.bankBalance;
 	}
-	public double getBankAPR() {
+	public double getBankAPR() 
+	{
 		return this.bankAPR;
 	}
 	
 		
 	public static void main(String[] args) {
-		Bank chase = new Bank("Chase", 6000, 0.1699);
-		System.out.println(chase.getBankName());
-		System.out.println(chase.getBankBalance());
-		System.out.println(chase.getBankAPR());		
+//		// Testing
+//		Bank chase = new Bank("Chase", 6000, 0.1699);
+//		System.out.println(chase.getBankName());
+//		System.out.println(chase.getBankBalance());
+//		System.out.println(chase.getBankAPR());		
+		Bank bank = new Bank();
+		
+		//Prompt for bank name
+		System.out.print("Enter bank name: ");
+		input = new Scanner(System.in);
+		String bankName = input.nextLine();
+		System.out.println(bankName);
+		bank.setBankName(bankName);
+		
+		//Prompt for bank balance
+		System.out.print("Enter bank balance: ");
+		String bankBalance = input.nextLine();
+		int bankBalanceInt = Integer.parseInt(bankBalance);
+		System.out.println(bankBalanceInt);
+		bank.setBankBalance((double) bankBalanceInt);
+		
+		//Prompt for bank APR
+		System.out.print("Enter bank APR: ");
+		String bankAPR = input.nextLine();
+		double bankAPRDouble = Double.parseDouble(bankAPR);
+		System.out.println(bankAPRDouble);
+		bank.setBankAPR(bankAPRDouble);
+
 	}
 
 }
