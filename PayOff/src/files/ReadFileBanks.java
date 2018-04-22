21ac88261ee898092926573a483b3cf356d3f854
Juan Carlo Pascua <carlo.pascua@gmail.com>
2018-04-21 22:54:37 -0700

    Updated ReadFileBanks.java to read bank files

--- PayOff/src/files/ReadFileBanks.java
+++ PayOff/src/files/ReadFileBanks.java
@@ -8,16 +8,26 @@ import java.util.Scanner;
 
 public class ReadFileBanks extends ReadFile {
 	private static Scanner input;
-
+	private static String[] stringArray;
 	public static void main(String[] args) throws FileNotFoundException{
-		String fileName = "";
+		stringArray = new String[4];
+		String fileName = "Bank of America.txt";
 		Path path = Paths.get(fileName);
 		File file = path.toFile();
 		input = new Scanner(file);
-		input = input.useDelimiter(";");
+		input = input.useDelimiter("\\n");
+		int i = 0;
 		while (input.hasNext()) {
 			String text = input.next().trim();
-			System.out.println(text);
+			stringArray[i] = text;
+			i++;
 		}
+		System.out.println("The Bank Name is: " + stringArray[0]);
+		System.out.println("The Bank Balance is: " + stringArray[1]);
+		System.out.println("The Bank Percentage is: " + stringArray[2]);
+		System.out.println("The Bank APR: " + stringArray[3]);
+
+
+
 	}// close main method
 }
