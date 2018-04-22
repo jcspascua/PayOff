9484004e4384beddaa570bbdb6984e2231f9ee33
Juan Carlo Pascua <carlo.pascua@gmail.com>
2018-04-21 22:45:30 -0700

    Added ReadFileBanks and ReadFileUser

--- PayOff/src/files/ReadFileBanks.java
+++ PayOff/src/files/ReadFileBanks.java
@@ -1,5 +1,23 @@
 package files;
 
-public class ReadFileBanks {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class ReadFileBanks extends ReadFile {
+	private static Scanner input;
+
+	public static void main(String[] args) throws FileNotFoundException{
+		String fileName = "";
+		Path path = Paths.get(fileName);
+		File file = path.toFile();
+		input = new Scanner(file);
+		input = input.useDelimiter(";");
+		while (input.hasNext()) {
+			String text = input.next().trim();
+			System.out.println(text);
+		}
+	}// close main method
 }
--- PayOff/src/files/ReadFileUser.java
+++ PayOff/src/files/ReadFileUser.java
@@ -1,5 +1,25 @@
 package files;
 
-public class ReadFileUser {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class ReadFileUser extends ReadFile {
+	private static Scanner input;
+
+	//Method that allows you to input a search for the user you want
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		String fileName = "";
+		Path path = Paths.get(fileName);
+		File file = path.toFile();
+		input = new Scanner(file);
+		input = input.useDelimiter(";");
+		while (input.hasNext()) {
+			String text = input.next().trim();
+			System.out.println(text);
+		}
+	}
 }
